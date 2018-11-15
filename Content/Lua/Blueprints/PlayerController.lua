caa2108082dd65a5564aa236f6446e0faa24c7cf
Jash <newzeadev@gmail.com>
2018-11-15 21:27:51 +0800

    Update PlayerController.lua

--- Content/Lua/Blueprints/PlayerController.lua
+++ Content/Lua/Blueprints/PlayerController.lua
@@ -2,6 +2,8 @@ local m = {}
 
 local Super = Super
 local LoadClass = LoadClass
+local CreateDelegate = CreateDelegate
+local CreateLatentAction = CreateLatentAction
 
 local GameplayStatics = LoadClass('GameplayStatics')
 local BlueluaLibrary = LoadClass('BlueluaLibrary')
@@ -19,7 +21,7 @@ function m:PlaySkippableCutscene(SequencePlayer)
     local WBSkipIntroClass = LoadClass('/Game/Blueprints/WidgetBP/WB_SkipIntro.WB_SkipIntro_C')
     self.SkipIntroWidget = WidgetBlueprintLibrary:Create(WorldContextObject, WBSkipIntroClass, Super)
     self.SkipIntroWidget:AddToViewport(0)
-    Super:ShowHUD(false)
+    self:ShowHUD(false)
 end
 
 function m:StopPlayingSkippableCutscene()
@@ -29,13 +31,78 @@ function m:StopPlayingSkippableCutscene()
     self.StopPlayingSkippableCutsceneOnce = true
 
     Super.SequencePlayer:Stop()
-    Super:ShowHUD(true)
+    self:ShowHUD(true)
 
     local GameMode = GameplayStatics:GetGameMode(WorldContextObject)
     GameMode:ToLuaObject():StartGame()
 
     self.SkipIntroWidget:RemoveFromParent()
     self.SkipIntroWidget = nil
+end
+
+function m:ShowHUD(bShow)
+    local bRunningOnMobile = self:IsRunningOnMobile()
+    Super:SetVirtualJoystickVisibility(bShow and bRunningOnMobile)
+
+    --TODO: move to common lua
+    local ESlateVisibility = {
+        Visible = 0,
+        Collapsed = 1,
+        Hidden = 2,
+        HitTestInvisible = 3,
+        SelfHitTestInvisible = 4,
+    }
+
+    if Super.OnScreenControls then
+        Super.OnScreenControls:SetVisibility(bShow and ESlateVisibility.SelfHitTestInvisible or ESlateVisibility.Hidden)
+    end
+end
+
+function m:CreateHUD()
+    if Super.OnScreenControls then
+        self:ShowHUD(true)
+        return
+    end
+    
+    local WidgetBlueprintLibrary = LoadClass('WidgetBlueprintLibrary')
+    local WBOnScreenControlsClass = LoadClass('/Game/Blueprints/WidgetBP/WB_OnScreenControls.WB_OnScreenControls_C')
+    Super.OnScreenControls = WidgetBlueprintLibrary:Create(WorldContextObject, WBOnScreenControlsClass, Super)
+    Super.OnScreenControls:AddToViewport(0)
+    Super.OnScreenControls:UpdateCurrentIcons()
+end
+
+function m:ReceivePossess(NewPawn)
+    if self.ReceivePossessOnce then
+        return
+    end
+    self.ReceivePossessOnce = true
+
+    Super.PlayerCharacter = NewPawn
+    self:CreateHUD()
+
+    Super.OnInventoryItemChanged:Add(self, self.OnInventoryItemChanged)
+
+    local LatentActionInfo = CreateLatentAction(CreateDelegate(Super,
+        function()
+            Super.PlayerCharacter:CreateAllWeapons()
+        end))
+
+    KismetSystemLibrary:Delay(WorldContextObject, 0.025, LatentActionInfo)
+end
+
+-- bool bAdded, URPGItem* Item
+function m:OnInventoryItemChanged(bAdded, Item)
+    Super:HandleInventoryItemChanged(bAdded, Item)
+end
+
+--TODO: move to common lua
+function m:IsRunningOnMobile()
+    local PlatformName = GameplayStatics:GetPlatformName()
+    if PlatformName == 'Android' or PlatformName == 'IOS' then
+        return true, PlatformName
+    end
+
+    return false, PlatformName
 end
 
 return m
---
+++ README.md
@@ -0,0 +1,6 @@
+# Action RPG Lua Version #
+
+## TODO ##
+
+* Read Custom defined data table from data asset in BP_GameMode
+* HandleInventoryItemChanged in BP_PlayerController
