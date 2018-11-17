5eb1793e7561f6d5251b3278fb35991cd6c2f4e1
Jash <newzeadev@gmail.com>
2018-11-17 18:28:00 +0800

    PauseMenu界面逻辑移到lua里

---
+++ Content/Lua/Blueprints/WidgetBP/WB_PauseMenu.lua
@@ -0,0 +1,53 @@
+local m = {}
+
+local Super = Super
+local LoadClass = LoadClass
+local CreateDelegate = CreateDelegate
+local CreateLatentAction = CreateLatentAction
+
+local GameplayStatics = LoadClass('GameplayStatics')
+local KismetSystemLibrary = LoadClass('KismetSystemLibrary')
+local BlueluaLibrary = LoadClass('BlueluaLibrary')
+
+local WorldContextObject = BlueluaLibrary:GetWorldContext()
+
+--TODO: move to common lua
+local EUMGSequencePlayMode = {
+    Forward = 0,
+    Reverse = 1,
+    PingPong = 2,
+}
+
+function m:Construct()
+    Super:PlayAnimation(Super.FadeAnimation, 0, 1, EUMGSequencePlayMode.Forward, 1)
+    Super.CloseButton.OnClicked:Add(self, self.OnCloseButtonClicked)
+    Super.OptionsButton.OnClicked:Add(self, self.OnOptionsButtonClicked)
+    Super.MainMenuButton.OnClicked:Add(self, self.OnMainMenuButtonClicked)
+end
+
+function m:OnCloseButtonClicked()
+    Super:PlayAnimation(Super.FadeAnimation, 0, 1, EUMGSequencePlayMode.Reverse, 1)
+
+    local LatentActionInfo = CreateLatentAction(CreateDelegate(Super,
+        function()
+            local GameMode = GameplayStatics:GetGameMode(WorldContextObject)
+            GameMode:PauseGame()
+            
+            Super:RemoveFromParent()
+        end))
+
+    KismetSystemLibrary:Delay(WorldContextObject, Super.FadeAnimation:GetEndTime(), LatentActionInfo)
+end
+
+function m:OnOptionsButtonClicked()
+    local WidgetBlueprintLibrary = LoadClass('WidgetBlueprintLibrary')
+    local WBOptionsScreenClass = LoadClass('/Game/Blueprints/WidgetBP/WB_OptionsScreen.WB_OptionsScreen_C')
+    local OptionsScreen = WidgetBlueprintLibrary:Create(WorldContextObject, WBOptionsScreenClass, nil)
+    OptionsScreen:AddToViewport(0)
+end
+
+function m:OnMainMenuButtonClicked()
+    GameplayStatics:OpenLevel(WorldContextObject, 'ActionRPG_Main', true, nil)
+end
+
+return m
