1337b8071379b0f43ee6be7e9566543ae31b15f5
Jash <newzeadev@gmail.com>
2018-11-17 11:24:18 +0800

    SkipIntro界面逻辑移到lua中

---
+++ Content/Lua/Blueprints/WidgetBP/WB_SkipIntro.lua
@@ -0,0 +1,27 @@
+local m = {}
+
+local Super = Super
+
+function m:Construct()
+    Super.SkipButton.OnClicked:Add(
+        function ()
+            local BlueluaLibrary = LoadClass('BlueluaLibrary')
+            local GameplayStatics = LoadClass('GameplayStatics')
+            
+            local WorldContextObject = BlueluaLibrary:GetWorldContext()
+            local PlayerController = GameplayStatics:GetPlayerController(WorldContextObject, 0):ToLuaObject()
+            PlayerController:StopPlayingSkippableCutscene()
+            Super:RemoveFromParent()
+        end)
+
+    --TODO: move to common lua
+    local EUMGSequencePlayMode = {
+        Forward = 0,
+        Reverse = 1,
+        PingPong = 2,
+    }
+    
+    Super:PlayAnimation(Super.SkipTextAnim, 0, 0, EUMGSequencePlayMode.Forward, 0.5)
+end
+
+return m
