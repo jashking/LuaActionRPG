d65af0516bae7b99c4a466a7625169a1384997fd
Jash <newzeadev@gmail.com>
2018-11-22 17:44:41 +0800

    InventoryItem to lua

---
+++ Content/Lua/Blueprints/WidgetBP/Inventory/WB_InventoryItem.lua
@@ -0,0 +1,31 @@
+local m = {}
+
+-- parent UObject
+local Super = Super
+
+-- global functions
+local LoadClass = LoadClass
+
+-- C++ library
+local GameplayStatics = LoadClass('GameplayStatics')
+
+-- Common
+local Common = require 'Lua.Blueprints.Common'
+
+function m:Construct()
+    Super.ItemImage:SetBrush(Super.ItemClass.ItemIcon)
+    Super.ItemName:SetText(Super.ItemClass.ItemName)
+    Super.LongDescription:SetText(Super.ItemClass.ItemDescription)
+
+    Super.InventoryButton.OnClicked:Add(self, self.OnInventoryButtonClicked)
+end
+
+function m:OnInventoryButtonClicked()
+    Super.OwningList.EquipmentButton:UpdateEquipmentSlot(Super.ItemClass)
+    Super.OwningList:CloseList()
+
+    local PlayerController = GameplayStatics:GetPlayerController(Super, 0)
+    PlayerController:SaveInventory()
+end
+
+return m
