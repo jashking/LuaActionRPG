4e78a341666af31acb523476338f14f031ee53f1
Jash <newzeadev@gmail.com>
2018-11-22 19:31:39 +0800

    EquipmentSlot to lua

---
+++ Content/Lua/Blueprints/WidgetBP/Inventory/WB_EquipmentSlot.lua
@@ -0,0 +1,65 @@
+local m = {}
+
+-- parent UObject
+local Super = Super
+
+-- global functions
+local LoadClass = LoadClass
+
+-- C++ library
+local GameplayStatics = LoadClass('GameplayStatics')
+
+-- Common
+local Common = require 'Lua.Blueprints.Common'
+
+function m:Construct()
+    self:RefreshItem_lua()
+
+    local PlayerController = GameplayStatics:GetPlayerController(Super, 0)
+    PlayerController.OnSlottedItemChanged:Add(self, self.OnSlottedItemChanged)
+
+    Super.SlotTypeLabel:SetText(Super.EquipSlot.ItemType.Name)
+    Super.EquipButton.OnClicked:Add(self, self.OnEquipButtonClicked)
+end
+
+function m:RefreshItem_lua()
+    local PlayerController = GameplayStatics:GetPlayerController(Super, 0)
+    local Item = PlayerController:GetSlottedItem(Super.EquipSlot)
+
+    Super.ItemImage:SetBrush(Item and Item.ItemIcon or Super.DefaultBrush)
+end
+
+function m:OnSlottedItemChanged(ItemSlot, Item)
+    local RPGBlueprintLibrary = LoadClass('RPGBlueprintLibrary')
+    if RPGBlueprintLibrary:EqualEqual_RPGItemSlot(ItemSlot, Super.EquipSlot) then
+        self:RefreshItem_lua()
+    end
+end
+
+function m:OnEquipButtonClicked()
+    local WBInventoryListClass = LoadClass('/Game/Blueprints/WidgetBP/Inventory/WB_InventoryList.WB_InventoryList_C')
+    local OriginalEquipmentButton = WBInventoryListClass.EquipmentButton
+    local OriginalItemType = WBInventoryListClass.ItemType
+
+    WBInventoryListClass.EquipmentButton = Super
+    WBInventoryListClass.ItemType = Super.EquipSlot.ItemType
+
+    local WidgetBlueprintLibrary = LoadClass('WidgetBlueprintLibrary')
+    local WBInventoryList = WidgetBlueprintLibrary:Create(Super, WBInventoryListClass, nil)
+    WBInventoryList:AddToViewport(0)
+
+    WBInventoryListClass.EquipmentButton = OriginalEquipmentButton
+    WBInventoryListClass.ItemType = OriginalItemType
+end
+
+function m:UpdateEquipmentSlot_lua(Item)
+    local PlayerController = GameplayStatics:GetPlayerController(Super, 0)
+    PlayerController:SetSlottedItem(Super.EquipSlot, Item)
+
+    self:RefreshItem_lua()
+
+    local GameInstance = GameplayStatics:GetGameInstance(Super)
+    GameInstance:WriteSaveGame()
+end
+
+return m
