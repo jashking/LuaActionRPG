8b6c413e0ab72fdbef6cff5d0af2571ed37a6490
Jash <newzeadev@gmail.com>
2019-02-18 17:32:00 +0800

    Update ios build bat

--- Build/Build_Game_iOS.bat
+++ Build/Build_Game_iOS.bat
@@ -1,5 +1,8 @@
 @echo off
 
+rem comment Agenda.DotNetProjects.Add(@"Engine\Source\Programs\IOS\iPhonePackager\iPhonePackager.csproj") in IOSPlatform.Automation.cs if build failed
+rem see https://github.com/EpicGames/UnrealEngine/commit/454ac079c3f2bd7eb0b4018e18ff7cf5989e2170
+
 set Target="Build Game iOS"
 set IterativeCooking=true
 set BuildConfiguration=Development
